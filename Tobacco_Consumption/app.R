e16519348a53d43a3c0161cc86a13f739d161bf4
Scott Schumacker <scottschu97@gmail.com>
2024-09-25 19:18:03 -0400

    Updated links, graph axis titles, and cleaned code formatting

--- Tobacco_Consumption/app.R
+++ Tobacco_Consumption/app.R
@@ -51,7 +51,7 @@ ui <- page_navbar(
     ),
     # Plot section
     layout_columns(cards[[1]], navset_card_underline(
-      title = "Combustible Tobacco Consumption Per Capita",
+      title = "Combustible Tobacco Consumption Per Capita (cigarette equivalents)",
       nav_panel("Consumption", plotlyOutput("consumptionTime")),
       nav_panel("Linear Regression Forecast", plotlyOutput("forecastPlot"))
     )),
@@ -60,7 +60,7 @@ ui <- page_navbar(
       nav_panel("Percentage change", plotlyOutput("changePlot")),
       nav_panel("Rate of change", plotlyOutput("ratePlot"))
     ), navset_card_underline(
-      title = "Tobacco Submeasure Comparison",
+      title = "Type Comparison (consumption per capita)",
       nav_panel("Cigars", plotlyOutput("cigarPlot")),
       nav_panel("Cigarettes", plotlyOutput("cigarettePlot")),
       nav_panel("Others", plotlyOutput("otherPlot"))
@@ -79,9 +79,12 @@ ui <- page_navbar(
       p("Data Set: Adult Tobacco Consumption In The U.S., 2000-Present"),
       p("Data Source: Data.gov"),
       p("Data License:"),
-      a(href = "https://opendatacommons.org/licenses/by/1-0/", "license"),
-      p("Disclaimer: This dashboard is for informal exploratory purposes only. 
-        This dashboard is not meant to be used for formal conclusions, publications, or formal research.")
+      a(href = "https://opendatacommons.org/licenses/by/1-0/", "license",
+        target = "_blank"),
+      strong("Disclaimer: "),
+      p("This dashboard is for informal exploratory purposes only. 
+        This dashboard is not meant to be used for formal conclusions, 
+        publications, or formal research.")
     )
   ),
   # Links section
@@ -97,7 +100,9 @@ server <- function(input, output){
   showModal(modalDialog(
     title = "Welcome to the Tobacco Consumption Dashboard!",
     paste0("This dashboard is to help you explore U.S. tobacco consumption data. ",
-    "Disclaimer: This dashboard is for informal exploratory purposes only.")))
+    "Disclaimer: This dashboard is for informal exploratory purposes only. 
+    This dashboard is not meant to be used for formal conclusions, 
+    publications, or formal research.")))
   
   # Data Load and Transformation
   # Loading data
@@ -120,44 +125,44 @@ server <- function(input, output){
     subset(tobacco_DF, Measure %in% c("Cigars") & 
              Submeasure %in% c("Small Cigars", "Large Cigars"))
   
-  newSubsetDF <- subset(tobacco_DF, Measure %in% c("Loose Tobacco", 
+  new_subset_DF <- subset(tobacco_DF, Measure %in% c("Loose Tobacco", 
                                                   "Smokeless Tobacco") &
                           Submeasure %in% c("Chewing Tobacco", "Pipe Tobacco", 
                                             "Roll-Your-Own Tobacco") &
                           `Data Value Unit` %in% c("Pounds"))
   
-  cigaretteDF <- subset(tobacco_DF, Submeasure %in% c("Cigarette Removals"))
+  cigarette_DF <- subset(tobacco_DF, Submeasure %in% c("Cigarette Removals"))
   
   # Creating Key Metrics - 7 T cigarette equivalents consumed
-  totalConsumption <- sum(combustible_DF$Total)
-  totalConsumption <- totalConsumption/1000000000000
+  total_consumption <- sum(combustible_DF$Total)
+  total_consumption <- total_consumption/1000000000000
   
   output$total <- renderText({
-    paste0(round(totalConsumption,2), " T")
+    paste0(round(total_consumption,2), " T")
   })
   
   # Total Consumed per capita in 2000
   total2000 <- subset(combustible_DF, Year == "2000", 
                       select = c("Total Per Capita"))
-  firstTotal <- total2000$`Total Per Capita`
+  first_total <- total2000$`Total Per Capita`
   
   # Total Consumed per capita in 2023
   total2023 <- subset(combustible_DF, Year == "2023", 
                       select = c("Total Per Capita"))
-  secondTotal <- total2023$`Total Per Capita`
+  second_total <- total2023$`Total Per Capita`
   
   # Creating KPI output for 2023 consumed per capita
   output$consume2023 <- renderText({
-    secondTotal
+    second_total
   })
   
   # Calculating Percent Change for key metric
-  percentChange <- round(((secondTotal - firstTotal)/firstTotal)*100,2)
-  percentChange
+  percent_change <- round(((second_total - first_total)/first_total)*100,2)
+  percent_change
   
   # Creating KPI output for percent change from 2000-2023
   output$change <- renderText({
-    paste0(round(percentChange,2), "%")
+    paste0(round(percent_change,2), "%")
   })
   
   # Creating output for population plot
@@ -180,36 +185,36 @@ server <- function(input, output){
         geom_point() +
         geom_line() +
         theme_bw() +
-        ylab("Cigarette Equivalents")
+        ylab("")
     )
   })
   
   # Creating linear regression forecast model
-  tobaccoModel <- lm(`Total Per Capita` ~ Year, data = combustible_DF)
-  tobaccoModel
+  tobacco_model <- lm(`Total Per Capita` ~ Year, data = combustible_DF)
+  tobacco_model
   
   # Creating prediction data frame to use for added predicted values to plot
   Year <- c(2024,2025,2026,2027,2028,2029,2030,2031,2032,2033)
   `Total Per Capita` <- NA
   futureYearDF <- data.frame(Year, `Total Per Capita`)
   colnames(futureYearDF) <- c("Year", "Total Per Capita")
-  predictionDF <- subset(combustible_DF, select = c("Year", "Total Per Capita"))
-  predictionDF <- rbind(predictionDF, futureYearDF)
-  predictionDF$predicted_value <- predict(tobaccoModel, predictionDF)
+  prediction_DF <- subset(combustible_DF, select = c("Year", "Total Per Capita"))
+  prediction_DF <- rbind(prediction_DF, futureYearDF)
+  prediction_DF$predicted_value <- predict(tobacco_model, prediction_DF)
   
   # Creating color palette
-  myColors <- c("#18BC9C", "#22577A", "#F7A072")
+  my_colors <- c("#18BC9C", "#22577A", "#F7A072")
   
   # Forecast plot
   output$forecastPlot <- renderPlotly({
     ggplotly(
-      ggplot(predictionDF, aes(Year, `Total Per Capita`)) +
+      ggplot(prediction_DF, aes(Year, `Total Per Capita`)) +
         geom_point() +
         geom_line(alpha = 0.2) +
         geom_smooth(method = "lm") +
         geom_point(aes(Year, predicted_value), color = "red", alpha = 0.3) +
         theme_bw() +
-        ylab("Cigarette Equivalents")
+        ylab("")
     )
   })
   
@@ -242,31 +247,31 @@ server <- function(input, output){
     ggplotly(
       ggplot(cigarSubset, aes(Year, `Total Per Capita`, fill = Submeasure)) +
         geom_bar(stat = "identity") +
-        scale_fill_manual(values = myColors) +
-        theme_bw() + 
-        ylab("Total Consumption Per Capita")
+        scale_fill_manual(values = my_colors) +
+        theme_bw() +
+        ylab("")
     )
   })
   
   # Cigarette plot
   output$cigarettePlot <- renderPlotly({
     ggplotly(
-      ggplot(cigaretteDF, aes(Year, `Total Per Capita`, fill = Submeasure)) +
+      ggplot(cigarette_DF, aes(Year, `Total Per Capita`, fill = Submeasure)) +
         geom_bar(stat = "identity") +
-        scale_fill_manual(values = myColors) +
+        scale_fill_manual(values = my_colors) +
         theme_bw() +
-        ylab("Total Consumption Per Capita")
+        ylab("")
     )
   })
   
   # Other plot
   output$otherPlot <- renderPlotly({
     ggplotly(
-      ggplot(newSubsetDF, aes(Year, `Total Per Capita`, fill = Submeasure)) +
+      ggplot(new_subset_DF, aes(Year, `Total Per Capita`, fill = Submeasure)) +
         geom_bar(stat = "identity") +
-        scale_fill_manual(values = myColors) +
+        scale_fill_manual(values = my_colors) +
         theme_bw() +
-        ylab("Total Consumption Per Capita")
+        ylab("")
     )
   })
 }
